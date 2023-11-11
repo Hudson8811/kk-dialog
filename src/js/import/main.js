6d506593c0dd3872abd3680da1078b47b385989b
kihhmaster <kihhmaster@yandex.ru>
2023-11-11 18:23:41 +0300

    contacts, customers,articles,article

--- src/js/import/main.js
+++ src/js/import/main.js
@@ -1,6 +1,8 @@
 jQuery(function () { 
 
 
+
+
 	function bodyNoScroll() {
 		let bodyBodymotionless = document.querySelector('body')
 		bodyBodymotionless.classList.add("Bodymotionless")
@@ -278,8 +280,76 @@ jQuery(function () {
 	});
 
 
+
+	
+
 });
 
+$(window).scroll(function() {
+	var height = $(window).scrollTop();
+	 /*Если сделали скролл на 100px задаём новый класс для header*/
+	if(height > 50){
+		$('.section__header').addClass('bg__white');
+	} else{
+	/*Если меньше 100px удаляем класс для header*/
+		$('.section__header').removeClass('bg__white');
+	}
+});
+
+
+function MapInit() {
+
+	if ($('.contacts__map').length > 0) {
+		const map = new ymaps.Map('js-map', {
+			center: [55.672925, 37.625181],
+			zoom:13,
+			// type: 'yandex#satellite',
+			// Карта будет создана без
+			// элементов управления.
+			controls: []
+		});
+	myPlacemark2 = new ymaps.Placemark([55.672925, 37.625181],{},{
+			iconLayout: 'default#image',
+			iconImageHref: '/img/svg/predstvo_act.svg',
+			iconImageSize: [30, 36],
+			iconImageOffset: [-15, -18]
+	});
+
+	// Добавляем метку на карту.
+	map.geoObjects.add(myPlacemark2);
+	$(".contacts__office").on("click", function () {
+			$('.contacts__office').each(function() {
+				var office = $(this);
+				office.removeClass( "active" )
+			});
+			$(this).addClass( "active" );
+			var coor = $(this).attr('data-coords');
+			console.log (coor)
+			var a = coor;
+			var xy = a.split(',');
+			var x = parseFloat(xy[0]);
+			var y = parseFloat(xy[1]);
+
+
+			
+			myPlacemark2 = new ymaps.Placemark([x, y],{},{
+					iconLayout: 'default#image',
+					iconImageHref: '../img/svg/predstvo_act.svg',
+					iconImageSize: [26, 26],
+					iconImageOffset: [-13, -13]
+			});
+
+			// Добавляем метку на карту.
+			map.geoObjects.add(myPlacemark2);
+			map.setCenter([x, y]);
+
+	}) 
+	
+	}
+	
+
+	
+}
 
 if (window.innerWidth > 992){
 	window.addEventListener('scroll', event => {
