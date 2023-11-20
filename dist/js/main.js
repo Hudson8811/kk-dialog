b29dc86f7d9f2f222577aac82ecc41d8c32f40a0
kihhmaster <kihhmaster@yandex.ru>
2023-11-20 17:40:21 +0300

    one

--- dist/js/main.js
+++ dist/js/main.js
@@ -164,15 +164,11 @@ $('.product-card__slider').each(function () {
   group.find('.gallery-thumbs').each(function () {
       var slider = $(this)
       galleryThumbs = new Swiper(slider[0], {
-          centeredSlides: true,
-          centeredSlidesBounds: true,
-          slidesPerView: 3,
+          slidesPerView: "auto",
           watchOverflow: true,
           watchSlidesVisibility: true,
           watchSlidesProgress: true,
           direction: 'horizontal',
-          centeredSlides: false,
-          centeredSlidesBounds: false,
           autoHeight: true,
           spaceBetween: 20,
           navigation: {
@@ -213,8 +209,52 @@ $('.product-card__slider').each(function () {
           //     },
           // }
       })
+
     //   galleryMain.controller.control = galleryThumbs;
   });
+  var wrap = $(this).closest('.product-card__section');
+  wrap.find('.product-card__color__filter__item').on( 'click', function() {
+    var filter = $(this).attr('data-filter');
+    $(".product-card__color__filter__item").removeClass("active");
+    if(filter==='all'){
+        wrap.find('.swiper-slide').css('display', '');
+        wrap.find('.color-list__selected__element').css('display', '');
+    }
+    else{
+        wrap.find('.swiper-slide').css('display', 'none');
+        wrap.find('.swiper-slide[data-filter="' + filter+'"').css('display', '').addClass("active");
+        wrap.find('.color-list__selected__element').css('display', 'none');
+        wrap.find('.color-list__selected__element[data-filter="' + filter+'"').css('display', '').addClass("active");
+        
+        
+    }
+
+    // $.not(this).removeClass('news__categories-item--active');
+    $( this ).addClass('active' );
+
+    function updateGalleryMain() {
+      galleryMain.updateSize();
+      galleryMain.updateSlides();
+      galleryMain.updateProgress();
+      galleryMain.updateSlidesClasses();
+      galleryMain.slideTo(0);
+      galleryMain.scrollbar.updateSize();
+    }
+    function updateGalleryThumbs() {
+      galleryThumbs.updateSize();
+      galleryThumbs.updateSlides();
+      galleryThumbs.updateProgress();
+      galleryThumbs.updateSlidesClasses();
+      galleryThumbs.slideTo(0);
+      galleryThumbs.scrollbar.updateSize();
+    }
+    updateGalleryMain();
+    updateGalleryThumbs();
+
+    return false;
+  });
+  wrap.find('.product-card__color__filter__item').first().next().trigger("click")
+
 
 //   galleryThumbs.controller.control = galleryMain;
   galleryMain.controller.control = galleryThumbs;
